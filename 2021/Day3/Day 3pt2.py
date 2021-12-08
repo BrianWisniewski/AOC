b782c8512a3b3195190bc320407980c06269c538
Brian Wisniewski <minidgenius@gmail.com>
2021-12-08 01:01:38 -0500

    Fucked this shit up

--- 2021/Day3/Day 3pt2.py
+++ 2021/Day3/Day 3pt2.py
@@ -6,27 +6,36 @@ ones = []
 zeros = []
 tmpNum = 0
 
-with open("2021\day3.txt") as f:
+with open("2021\Day3\day3.txt") as f:
     data = f.readlines()
-    
+
 newData = copy.deepcopy(data)
+data = copy.deepcopy(newData)
+print(len(data[0]))
 
 for pos in range(12):
-
     ones = 0
     zeros = 0
-    for line in newData:
+    for line in data:
         if line[pos] == "1":
             ones += 1
         else:
             zeros += 1
+    print(ones, zeros)
+
+
     commonValue = 1 if ones>=zeros else 0
     print(commonValue)
-    for i in newData:
-        if i[pos] == commonValue:
-            while i[pos] in newData:
-                newData.remove(i)
 
+
+    for i in data:
+        if i[pos] != commonValue:
+            try:
+                while True:
+                    newData.remove(i)
+            except ValueError:
+                pass
+    data = copy.deepcopy(newData)
 
 
 
