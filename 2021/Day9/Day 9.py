0fb2c86fe7d4e5a277459874904f0f0dfae646cb
Brian Wisniewski <brianwiz@icloud.com>
2021-12-09 11:48:59 -0500

    refactor, still not working

--- 2021/Day9/Day 9.py
+++ 2021/Day9/Day 9.py
@@ -1,10 +1,14 @@
-with open("2021\Day9\day9.txt") as f:
-    data = f.read()
-data = data.split("\n")
+import copy
 
 potentials = []
 done = []
 total = 0
+
+with open("2021\Day9\day9.txt") as f:
+    data = f.read()
+data = data.split("\n")
+
+
 
 for pos, line in enumerate(data):
     for i, num in enumerate(line):
@@ -32,71 +36,58 @@ for y, x, n in potentials:
         done.append((y, x, n))
         total+=(n+1) 
 
-print(done)
+
+basins = []
 
 for i, j, n in done:
-    working = []
-    working.append((i,j))
-    filled=True
-    while filled==False:
+
+    basin = [(i,j)]
+
+    filled=False
+    while not filled:
         filled = True
+        newWorking = []
+        working = []
+
         for y, x in working:
+
             try:
-                if (data[y][x-1] != 9) and ((y, x-1) not in working):
-                    working.append((y, x-1))
+                if data[y][x-1] != 9 and (y, x-1) not in basin:
+                    basin.append((y, x-1))
+                    newWorking.append((y, x-1))
                     filled = False
-                    print('test')
+
             except Exception:
                 pass
 
             try:
-                if data[y][x+1] != 9 and (y, x+1) not in working:
-                    working.append((y, x+1))
+                if data[y][x+1] != 9 and (y, x+1) not in basin:
+                    newWorking.append((y, x+1))
+                    basin.append((y, x+1))
                     filled = False
+
             except Exception:
                 pass
 
             try:
-                if data[y-1][x-1] != 9 and (y-1, x-1) not in working:
-                    working.append((y-1, x-1))
+                if data[y+1][x] != 9 and (y+1, x) not in basin:
+                    newWorking.append((y+1, x))
+                    basin.append((y+1, x))
                     filled = False
+
             except Exception:
                 pass
 
             try:
-                if data[y-1][x] != 9 and (y-1, x) not in working:
-                    working.append((y-1, x))
-                    filled = False
+                if data[y-1][x] != 9 and (y-1, x) not in basin:
+                    newWorking.append((y-1, x))
+                    basin.append((y-1, x))
+                    filled = False    
+
             except Exception:
                 pass
 
-            try:
-                if data[y-1][x+1] != 9 and (y-1, x+1) not in working:
-                    working.append((y-1, x+1))
-                    filled = False
-            except Exception:
-                pass
-
-            try:
-                if data[y+1][x-1] != 9 and (y+1, x-1) not in working:
-                    working.append((y+1, x-1))
-                    filled = False
-            except Exception:
-                pass
-
-            try:
-                if data[y+1][x] != 9 and (y+1, x) not in working:
-                    working.append((y+1, x))
-                    filled = False
-            except Exception:
-                pass
-
-            try:
-                if data[y+1][x+1] != 9 and (y+1, x+1) not in working:
-                    working.append((y+1, x+1))
-                    filled = False                                                                                            
-            except Exception:
-                pass
-
-    print(working)
-    print(len(working))
+            working = copy.deepcopy(newWorking)
+            newWorking = []
+    basins.append(basin)
+    print(basin)
