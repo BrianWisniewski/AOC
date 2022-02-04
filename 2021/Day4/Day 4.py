a2fbf5faace97253efc1fad98427bc302f784c90
Brian Wisniewski <56554110+BrianWisniewski@users.noreply.github.com>
2022-02-04 19:42:38 +0000

    Started Day 4, needs debugging

--- 2021/Day4/Day 4.py
+++ 2021/Day4/Day 4.py
@@ -1,10 +1,50 @@
+import copy
+
+with open('./nums.txt') as f:
+  nums = f.read().split(',')
+
+with open('./boards.txt') as f:
+  boards = f.read().split('\n\n')
+
+for i, board in enumerate(copy.copy(boards)):
+  boards[i] = list(line.split() for line in board.split('\n'))
+print(boards)
 
 
-with open('2021/day4/day4Boards.txt') as f:
-    boards = f.read().split('\n\n')
+def hasWon(board, nums):
+  hasWon = False
 
-boards = []
+  #checks horizontal
+  for i in range(5):
+    if board[i].count(board[i][0]) == 5 and board[i][0] in nums:
+      hasWon = True
 
-for board in boards:
-    board.split('\n')
-    print(board)
+  #checks vertical
+  for i in range(5):
+    subList = []
+    for j in range(5):
+      subList.append(board[j][i])
+    if subList.count(board[i][0]) == 5 and subList[0] in nums:
+      hasWon = True
+
+  #checks diagnal
+  topLeft = board[0][0]
+  topRight = board[0][4]
+
+  for i in range(5):
+    if board[i][i] == topLeft and topLeft in nums:
+      if i == 4:
+        hasWon = True
+    else:
+      break
+
+    if board[i][-(i+1)] == topRight and topRight in nums:
+      if i == 4:
+        hasWon = True
+    else:
+      break
+    
+    return hasWon
+  
+for i in boards:
+  print(hasWon(i, nums))
