256d60f2a6fecdff85259cb032aa71adca807466
Brian Wisniewski <56554110+BrianWisniewski@users.noreply.github.com>
2022-02-16 05:06:19 +0000

    completed day 4 part 1 and learned some stuff about list comprehensions.

--- 2021/Day4/Day 4.py
+++ 2021/Day4/Day 4.py
@@ -8,43 +8,52 @@ with open('./boards.txt') as f:
 
 for i, board in enumerate(copy.copy(boards)):
   boards[i] = list(line.split() for line in board.split('\n'))
-print(boards)
-
 
 def hasWon(board, nums):
   hasWon = False
 
   #checks horizontal
   for i in range(5):
-    if board[i].count(board[i][0]) == 5 and board[i][0] in nums:
+    line = board[i]
+    inNums = 0
+    for num in line:
+      if num in nums:
+        inNums += 1
+    if inNums == 5:
       hasWon = True
 
   #checks vertical
   for i in range(5):
-    subList = []
-    for j in range(5):
-      subList.append(board[j][i])
-    if subList.count(board[i][0]) == 5 and subList[0] in nums:
+    col = [line[i] for line in board]
+    bingo = 0
+    for num in col:
+      if num in nums:
+        bingo += 1
+    if bingo == 5:
       hasWon = True
 
-  #checks diagnal
-  topLeft = board[0][0]
-  topRight = board[0][4]
+  return hasWon
 
-  for i in range(5):
-    if board[i][i] == topLeft and topLeft in nums:
-      if i == 4:
-        hasWon = True
-    else:
+def totalUnusedNums(board, nums):
+  total = 0
+  for line in board:
+    for num in line:
+      if num not in nums:
+        total += int(num)
+  return total
+        
+# This is jank and I know it.
+breakLoop = False
+for i, _ in enumerate(nums):
+  numRange = nums[:i+1]
+  for board in boards:
+    if hasWon(board, numRange):
+      out = (board, numRange)
+      breakLoop = True
       break
+  if breakLoop:
+    break
+winningBoard, winningNums = out
 
-    if board[i][-(i+1)] == topRight and topRight in nums:
-      if i == 4:
-        hasWon = True
-    else:
-      break
-    
-    return hasWon
-  
-for i in boards:
-  print(hasWon(i, nums))
+anwser = totalUnusedNums(winningBoard, winningNums) * int(winningNums[-1])
+print(anwser)
